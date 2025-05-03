2a573b2241d884478d619343539ad54ebe5b604f
Erlandsonjr <erlandsonjunior@outlook.com>
2025-05-03 01:13:53 -0300

    Adicionado cors

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import face_recognition
 import numpy as np
 import cv2
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/gerar-codificacao/")
 async def gerar_codificacao(file: UploadFile = File(...)):
