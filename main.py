e1fc6c7ee77e9ffeaf964cda4051bcf978abc81e
Erlandsonjr <erlandsonjunior@outlook.com>
2025-05-02 21:12:39 -0300

    Atualização da API

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, UploadFile, File
 import face_recognition
 import numpy as np
 import cv2
-from pymongo import MongoClient
 
 app = FastAPI()
 
-# Configuração do MongoDB
-MONGO_URI = "mongodb://localhost:27017"
-client = MongoClient(MONGO_URI)
-
-# Escolher o banco de dados e a coleção
-db = client["reconhecimento_facial"]
-collection = db["codificacoes"]
-
-@app.post("/reconhecer/")
-async def reconhecer(cpf: str = Form(...), file: UploadFile = File(...)):
+@app.post("/gerar-codificacao/")
+async def gerar_codificacao(file: UploadFile = File(...)):
     """
-    Endpoint para verificar se a codificação do rosto enviado corresponde ao CPF fornecido.
-    Retorna apenas um booleano em JSON.
+    Endpoint para receber uma imagem, gerar a codificação facial e retornar a codificação.
     """
-    dado = collection.find_one({"cpf": cpf}, {"_id": 0})
-    if not dado:
-        return False
-
-    codificacao_cpf = dado["codificacao"]
-
     conteudo = await file.read()
     nparr = np.frombuffer(conteudo, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
@@ -38,9 +22,7 @@ async def reconhecer(cpf: str = Form(...), file: UploadFile = File(...)):
 
     codificacoes = face_recognition.face_encodings(img_rgb, rostos)
 
-    for codificacao_rosto in codificacoes:
-        correspondencia = face_recognition.compare_faces([codificacao_cpf], codificacao_rosto, tolerance=0.5)
-        if correspondencia[0]:
-            return True
+    if not codificacoes:
+        return False
 
-    return False
+    return codificacoes[0].tolist()
